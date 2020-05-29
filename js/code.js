8d8295ee40ab48de9fcc68cdd5e45668b481bad9
NZ <63315741+nazar-yaremchuk@users.noreply.github.com>
2020-05-30 00:55:52 +0300

    Add files via upload

---
+++ js/code.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+$(document).ready(function () {
+    $("#menu").on("click", "a", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({ scrollTop: top }, 1500);
+    });
+});
